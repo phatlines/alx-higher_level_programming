83e1861b3865b41d6d58a46b73b72780b5835666
Muyiwa Fatunsin <106930236+phatlines@users.noreply.github.com>
2022-10-13 06:26:35 +0100

    Fixes for 2-Square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""defines a class Square"""
+
 
 class Square:
-    
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-
-
-        if not isinstance(size, int):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
