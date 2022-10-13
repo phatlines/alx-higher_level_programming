339d90361a4d0932b3ea18c3139d171db779d165
Muyiwa Fatunsin <106930236+phatlines@users.noreply.github.com>
2022-10-13 06:27:57 +0100

    Fixes  for 3-Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""defines a class Square"""
+
 
 class Square:
-    """Represent a square."""
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """Initialize a new square.
-
+        """initializes the square
         Args:
-            size (int): The size of the new square.
+            size (int): size of a side of the square
+        Returns:
+            None
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
