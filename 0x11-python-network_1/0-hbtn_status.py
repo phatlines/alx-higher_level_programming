b776b2e2ca52da0fbce22e06598fdc865c62bd2e
phatlines <muyiwafatunsin@gmail.com>
2023-01-18 07:46:35 -0800

    Checker errors fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,20 +2,13 @@
 """
 Script that fetches https://intranet.hbtn.io/status
 """
-import urllib.request
+from urllib import request
 
-
-def main():
-    """
-    Funtion to print a response of a specific url
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
 
 if __name__ == "__main__":
-    main()
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
