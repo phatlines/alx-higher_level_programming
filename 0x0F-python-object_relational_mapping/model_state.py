d660e4e3584acbddf4f88e7532fa3b5ffa921746
phatlines <muyiwafatunsin@gmail.com>
2023-02-01 09:12:16 -0800

    ModelState

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+Defines a state model that contain the class definition
+ of a State and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String, MetaData
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base()
 
 
 class State(Base):
     """
-    Class with id and name attributes of each state
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column
+     of an auto-generated, unique integer, can't
+      be null and is a primary key
+    class attribute name that represents a column
+     of a string with maximum 128 characters and
+      can't be null
+
     """
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
